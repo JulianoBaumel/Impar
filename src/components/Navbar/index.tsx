a47520cb58b9c33f4b4630079842052ba31fd94b
Julino Baumel <juliano_baumel@hotmail.com>
2022-08-24 09:31:08 -0300

    Modificações na barra de navegação

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -3,10 +3,34 @@ import logo from "../../assets/logo-teste.svg";
 function Navbar() {
 
   return (
-    <nav className="fixed bg-gradient-to-r from-primaria to-secundaria shadow-3xl w-screen">
-      <div className="max-w-8xl mx-auto px-6">
-        <div className="relative flex items-center h-16">
-          <img className="font-muli" src={logo}></img>
+    <nav className="
+      antialiased
+      z-10
+      fixed
+      bg-gradient-to-r 
+      from-primaria
+      to-secundaria
+      shadow-navbar-shadow
+      w-screen">
+      <div className="
+        max-w-8xl
+        mx-auto
+        px-6">
+        <div className="
+          relative 
+          flex 
+          items-center 
+          h-16">
+          <img src={logo}></img>
+            <div className="
+              font-muli 
+              text-24 
+            text-white
+              font-semibold
+              select-none">
+                <span className="mr-2">Teste</span>
+                <span className="font-light">Ímpar</span>
+            </div>
         </div>
       </div>
     </nav>
