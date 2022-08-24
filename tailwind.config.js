cd314bac5e53596933144533285219d00d687874
Julino Baumel <juliano_baumel@hotmail.com>
2022-08-24 09:29:08 -0300

    Transcrevendo CSS para o Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,34 @@
 module.exports = {
-  content: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
+  content: ['./public/*.{html}','./src/**/*.{js,jsx,ts,tsx,vue,html}','./src/assets/*.{svg}'],
   theme: {
+    screens: {
+      'xs': '390px', // => @media (min-width: 390px)
+      'sm': '640px', // => @media (min-width: 640px)
+      'md': '768px', // => @media (min-width: 768px)
+      'lg': '1024px', // => @media (min-width: 1024px)
+      'xl': '1280px', // => @media (min-width: 1280px)
+      '2xl': '1440px',// => @media (min-width: 1440px)
+    },
+    maxWidth: {
+      'max-w-8xl': '90rem',
+      '2/3': '66.666667%',
+    },
     fontFamily: {
-      'Muli': ['Muli', 'sans-serif']
+      'muli': ['Muli', 'sans-serif'],
     },
     fontSize: {
       '12': '.75rem',
       '13': '.813rem',
-      'base': '1rem',
       '18': '1.125rem',
       '21': '1.313rem',
       '24': '1.5rem',
       '32': '2rem',
-      '52': '3.25rem',
+      '52': '3.25rem'
     },
     extend: {
+      backgroundImage: {
+        'busca': "url('./src/assets/fundo-busca@2x.png')",
+      },
       colors: {
         'primaria': '#5F1478',
         'secundaria': '#AE276F',
@@ -25,16 +39,43 @@ module.exports = {
         'erro': '#DB2525',
         'sucesso': '#51C421',
         'texto': '#454545',
-        'destaque': '#E76316'
+        'destaque': '#E76316',
+
+        'primaria-text': '#757575',
+        'secundaria-text': '#263238',
+
+        'card-elipse': '#F6F6F6',
+        'card-border': '#E4E4E4',
+        'card-line-x': '#F0EFF0',
+        'card-line-y': '#F6F4F6',
+
+        'sidebar-bg':'#F6F4F6CC',
+        
+        '000000': '#000000',
+        'a02724': '#A02724',
+        'b55b52': '#B55B52',
+        'c64049': '#C64049',
+        'e6e6e6': '#E6E6E6',
+        'e0e0e0': '#E0E0E0',
+        'f5f5f5': '#F5F5F5',
+        'f0f0f0': '#F0F0F0',
+        'ebebeb': '#EBEBEB',
+        '6c63ff': '#6C63FF'
       },
       boxShadow: {
-        '3xl': '0px 3px 6px #00000029',
+        'navbar-shadow': '0px 3px 6px #00000029',
+        'card-shadow': '0px 3px 6px #E5E5E5;',
+        'card-shadow-inner': 'inset 0px 3px 6px #0000000F'
+      },
+      width: {
+        '1040': '65rem'
+      },
+      spacing:{
+        '25px': '1.563',
+        '13px': '0.813'
       }
     },
-    maxWidth: {
-      'max-w-8xl': '90rem'
-    }
   },
-  plugins: [
-],
+  plugins: [],
 }
+
