e31cdd85e6f1eb80b05cc46e9edeee23e1b59b05
Julino Baumel <juliano_baumel@hotmail.com>
2022-08-24 09:29:53 -0300

    Criação do componente botão

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,19 @@
+function Button() {
+
+  return (
+    <button type="button" className="
+      h-20
+      xs:h-10
+      md:h-12 
+      md:w-44 
+      rounded-lg 
+      bg-destaque 
+      text-white
+      text-18
+      font-bold">
+      Novo Card
+    </button>
+  );
+}
+
+export default Button;
