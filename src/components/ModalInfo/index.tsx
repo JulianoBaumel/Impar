3ea61e7141c407cc8cb11624188439a0788ee0eb
Julino Baumel <juliano_baumel@hotmail.com>
2022-09-02 09:00:24 -0300

    Modificação na Modal de Informações
    Correção na posição dos textos para melhor visualização.

--- src/components/ModalInfo/index.tsx
+++ src/components/ModalInfo/index.tsx
@@ -6,7 +6,7 @@ import PokemonImage from "../../services/PokemonImage";
 
 export default function ModalInfo(props: { name: string; visibility: any; }){
 
-    const [pokemon, setPokemon] = useState();
+    const [pokemon, setPokemon] = useState([]);
 
     console.log(props.name)
 
@@ -27,21 +27,51 @@ export default function ModalInfo(props: { name: string; visibility: any; }){
         fixed 
         inset-0 
         flex
-        justify-center items-center
-        bg-white bg-opacity-80">
+        justify-center 
+        items-center
+        bg-white 
+        bg-opacity-80
+        ">
             <div className="
-            p-2 
-            h-2/3 w-1/3
+            p-2
+            min-h-fit 
+            w-1/3
             min-w-min
             border
             bg-white 
-            text-texto text-12 
-            font-muli font-semibold uppercase">
-                <div className="relative flex flex-col items-center">
-                    <div className="absolute -top-6 -right-6">
+            text-texto 
+            text-12 
+            font-muli 
+            font-semibold 
+            uppercase
+            ">
+                <div className="
+                relative 
+                flex 
+                flex-col 
+                items-center
+                ">
+                    <div className="
+                    absolute 
+                    -top-6 
+                    -right-6
+                    ">
                         <div onClick={() => {props.visibility(false);}}  
-                            className="w-9 h-9 p-2 bg-destaque rounded-full shadow-close-modal-x hover:cursor-pointer">
-                            <svg viewBox="0 0 24 24" className="w-5 h-5 stroke-2 stroke-white">
+                            className="
+                            w-9 
+                            h-9 
+                            p-2 
+                            bg-destaque 
+                            rounded-full 
+                            shadow-close-modal-x 
+                            hover:cursor-pointer
+                            ">
+                            <svg viewBox="0 0 24 24" className="
+                            w-5 
+                            h-5 
+                            stroke-2 
+                            stroke-white
+                            ">
                                 <path d="M4.5 19.5l15-15m-15 0l15 15" />
                             </svg>
                         </div>
@@ -68,27 +98,28 @@ export default function ModalInfo(props: { name: string; visibility: any; }){
                         ))}
                     </div>
                     
-                    <div className="w-2/3 grid grid-cols-2 gap-1 place-items-center">
+                    <div className="w-3/5 grid grid-cols-2 gap-1 place-items-center mb-7">
 
-                        <div className="col-span-2 border-card-line-x border-b mb-3 mt-1 text-13">Information</div>
+                        <div className="w-full col-span-2 border-card-line-x border-b mb-3 mt-1 text-13 text-center">Information</div>
                         
-                        <div>Id:</div>
-                        <div>{pokemon.id}</div>
+                        <div className="w-full">Id:</div>
+                        <div className="w-full text-right">{pokemon.id}</div>
 
-                        <div>Nome:</div>
-                        <div>{pokemon.name}</div>
+                        <div className="w-full">Nome:</div>
+                        <div className="w-full text-right">{pokemon.name}</div>
 
-                        <div>Altura:</div>
-                        <div>{pokemon.height /10} m</div>
+                        <div className="w-full">Altura:</div>
+                        <div className="w-full text-right">{pokemon.height /10} m</div>
 
-                        <div>Peso:</div>
-                        <div>{pokemon.weight /10} kg</div>
+                        <div className="w-full">Peso:</div>
+                        <div className="w-full text-right">{pokemon.weight /10} kg</div>
 
-                        <div className="col-span-2 border-card-line-x border-b mb-3 mt-1 text-13">Stats</div>
+                        <div className="w-full col-span-2 border-card-line-x border-b mb-3 mt-1 text-13 text-center">Stats</div>
                         
                         {pokemon.stats.map((data: any) =>(
                             <>
-                                <div>{data.stat.name}:</div><div>{data.base_stat}</div>
+                                <div className="w-full">{data.stat.name}:</div>
+                                <div className="w-full text-right">{data.base_stat}</div>
                             </>
                         ))}
 
