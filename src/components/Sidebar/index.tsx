ec38dbd4835c974189652d79cee810057d78f40a
Julino Baumel <juliano_baumel@hotmail.com>
2022-09-02 16:07:18 -0300

    Modificado a Sidebar
    Removido responsabilidade;
    Indentado as classes do tailwind;
    Adicionado comentários.

--- src/components/Sidebar/index.tsx
+++ src/components/Sidebar/index.tsx
@@ -1,17 +1,14 @@
 import { useState } from "react";
 import create from '../../../src/assets/icone_criar.svg'
-import Button from '../Button';
+import Button from './Button';
+import InputFile from "./InputFile";
+import Input from "./Input";
 
 export default function Sidebar(props: { pokemons: any[]; setPokemons: any; visibility: any; }){
 
-  const [pokeName, setPokeName] = useState("");
-  const [pokeImg, setPokeImg] = useState();
-
-  function addPokemon() {
-    const data = Array.from(props.pokemons);
-    data.push({name: pokeName, src: pokeImg});
-    props.setPokemons(data);
-  }
+  //Guarda a Imagem e o Nome do Pokemon adicionado.
+  const [pokeName, setPokeName] = useState('');
+  const [pokeImg, setPokeImg] = useState('');
 
   return(
     <div className="
@@ -22,40 +19,58 @@ export default function Sidebar(props: { pokemons: any[]; setPokemons: any; visi
     inset-0
     flex
     justify-end
-    bg-white bg-opacity-80">
-        <div onClick={() => {props.visibility(false);}} className='grow'/>
+    bg-white bg-opacity-80
+    ">
+      <div onClick={() => {props.visibility(false);}} className='grow'/>
+      <div className="
+      z-30
+      h-full 
+      md:w-5/6
+      lg:w-3/6
+      xl:w-5/12
+      mb-1
+      py-10 px-9 
+      bg-white
+      ">
         <div className="
-          z-30
-          h-full 
-          sm:w-full
-          md:w-5/6
-          lg:w-3/6
-          xl:w-5/12
-          mb-1
-          py-10 px-9 
-          bg-white ">
-            <div className="flex space-x-px items-end w-2/3 pt-8 pb-9 min-w-fit">
-              <div className="flex-none w-12 h-12 mr-4">
-                <img src={create} className="" alt=""></img>
-              </div>
-              <div className="grow">
-                <label className="text-primaria font-bold text-32 leading-10 select-none">
-                Criar card
-                </label>
-              </div>
-            </div>
-            <div>
-              <span className="uppercase">Digite um nome para o card</span>
-              <input placeholder="Digite o título..." type="text" onChange={() => {(p) => setPokeName(p.target.value)}}/>
-            </div>
-            <div>
-              <span className="uppercase">inclua uma imagem para aparecer no card</span>
-              <input type="file" onChange={() => {(p) => setPokeImg(p.target.value)}}/>
-            </div>
-            <div className="flex" onClick={() => {props.visibility(false);}}>
-              <Button text={"Criar card"} answer={addPokemon}/>
-            </div>
+        min-w-fit
+        pt-8
+        pb-7
+        flex
+        ">
+          <div className="
+          w-12 
+          h-12 
+          mr-4
+          ">
+            <img src={create} alt=""></img>
+          </div>
+          <div className="grow">
+            <label className="
+            text-primaria 
+            font-bold 
+            text-32 
+            select-none
+            ">
+              Criar card
+            </label>
+          </div>
         </div>
+        <form className="
+        border-y
+        py-12
+        border-sidebar-border
+        text-13
+        font-bold
+        text-texto
+        ">
+          <Input onChange={setPokeName} value={pokeName}/>
+          <InputFile onChange={setPokeImg} value={pokeImg}/>
+        </form>
+        <div className="mt-7 flex justify-end">
+          <Button name={pokeName} image={pokeImg} pokemons={props.pokemons} setPokemons={props.setPokemons} visibility={props.visibility}/>
+        </div>
+      </div>
     </div>
   );
 }
