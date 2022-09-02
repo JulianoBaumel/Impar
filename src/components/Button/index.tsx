335f81072b65cb80d989e012fb5a9259132dda7f
Julino Baumel <juliano_baumel@hotmail.com>
2022-09-02 15:52:24 -0300

    Modificações no Botão
    Definido como um item único, definido apenas a responsabilidade de visibilidade da Sidebar.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,19 +1,19 @@
-export default function Button(props: {text: any, answer: any}){
+export default function Button(props: {setVisibilitySidebar: any}){
 
   return (
-    <button type="button" onClick={() => {props.answer(true);}}
-      className="
-      h-20
-      xs:h-10
-      md:h-12 
-      md:w-44 
-      rounded-lg 
-      bg-destaque 
-      text-white
-      text-18
-      font-bold
-      shadow-button">
-      {props.text}
+    <button type="button" onClick={() => {props.setVisibilitySidebar(true);}}
+    className="
+    h-20
+    xs:h-10
+    md:h-12 
+    md:w-44 
+    rounded-lg 
+    bg-destaque 
+    text-white
+    text-18
+    font-bold
+    shadow-button">
+      Novo Card
     </button>
   );
 
