1c2579e96f966439e9613f7ec9659d503147ac77
Julino Baumel <juliano_baumel@hotmail.com>
2022-09-02 16:03:21 -0300

    Indentação
     Indentação nas classes do tailwind.

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -4,32 +4,36 @@ export default function Navbar() {
 
   return (
     <nav className="
-      antialiased
+      font-muli
       z-10
       fixed
       bg-gradient-to-r 
       from-primaria
       to-secundaria
       shadow-navbar-shadow
-      w-screen">
+      w-screen
+      ">
       <div className="
-        max-w-8xl
-        mx-auto
-        px-6">
+      max-w-8xl
+      mx-auto
+      px-6
+      ">
         <div className="
-          relative 
-          flex 
-          items-center 
-          h-16">
-          <img src={logo}></img>
+        relative 
+        flex 
+        items-center 
+        h-16
+        ">
+          <img src={logo} className="mr-5"></img>
             <div className="
-              font-muli 
-              text-24 
+            -mt-1
+            text-24 
             text-white
-              font-semibold
-              select-none">
-                <span className="mr-2">Teste</span>
-                <span className="font-light">Ímpar</span>
+            font-semibold
+            select-none
+            ">
+              <span className="mr-2">Teste</span>
+              <span className="font-light">Ímpar</span>
             </div>
         </div>
       </div>
