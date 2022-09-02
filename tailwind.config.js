3912beace1c6aa861c2082383b72f7d5dc52b416
Julino Baumel <juliano_baumel@hotmail.com>
2022-09-02 16:00:41 -0300

    Comentários na configuração do Tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   content: ['.{html}','./src/**/*.{js,jsx,ts,tsx}','./src/assets/*.{svg}'],
   theme: {
+    //Modifica os tamanhos da tela predefinidos
     screens: {
       'xs': '390px', // => @media (min-width: 390px)
       'sm': '640px', // => @media (min-width: 640px)
@@ -9,12 +10,15 @@ module.exports = {
       'xl': '1280px', // => @media (min-width: 1280px)
       '2xl': '1440px',// => @media (min-width: 1440px)
     },
+    //Modifica o tamanho maximo predefinido
     maxWidth: {
       '2/3': '66.666667%',
     },
+    //Define a fonte tipográfica
     fontFamily: {
       'muli': ['Muli', 'sans-serif'],
     },
+    //Define as medidas das fontes
     fontSize: {
       '8': '.5rem',
       '12': '.75rem',
@@ -26,34 +30,43 @@ module.exports = {
       '52': '3.25rem'
     },
     extend: {
+      //Define a imagem de fundo
       backgroundImage: {
         'busca': "url('./src/assets/fundo-busca@2x.png')",
       },
+      //Define as cores com os nomes predefinidos, as que não tinham eu tomei a liberdade de escolher.
       colors: {
+        //Cores Principais
         'primaria': '#5F1478',
         'secundaria': '#AE276F',
         'stroke': '#EDD8F3',
         'bg': '#F9F0FC',
-        'branco': '#FFFFFF',
         'aviso': '#FFC400',
         'erro': '#DB2525',
         'sucesso': '#51C421',
         'texto': '#454545',
         'destaque': '#E76316',
 
+        //Textos
         'primaria-text': '#757575',
         'secundaria-text': '#263238',
+        
+        //Sidebar
+        'sidebar-border': '#D4D4D4',
+        'sidebar-input-border': '#B9B9B9',
+        'sidebar-bg':'#F6F4F6CC',
 
+        //Card
         'card-elipse': '#F6F6F6',
         'card-border': '#E4E4E4',
         'card-line-x': '#F0EFF0',
         'card-line-y': '#F6F4F6',
 
+        //Modal
         'modal-elipse-border': '#E4E4E4',
         'modal-elipse': '#DB25250F',
 
-        'sidebar-bg':'#F6F4F6CC',
-        
+        //Não utilizados
         '000000': '#000000',
         'a02724': '#A02724',
         'b55b52': '#B55B52',
@@ -65,20 +78,26 @@ module.exports = {
         'ebebeb': '#EBEBEB',
         '6c63ff': '#6C63FF'
       },
+      //Define as sombras
       boxShadow: {
         'navbar-shadow': '0px 3px 6px #00000029',
         'card-shadow': '0px 3px 6px #E5E5E5;',
         'card-shadow-inner': 'inset 0px 3px 6px #0000000F',
         'close-modal-x': '0px 3px 6px #E7631636',
-        'button' : '0px 3px 6px #92207242'
+        'button' : '0px 3px 6px #92207242',
+        'button-file' : '0px 3px 6px #E763162E'
       },
+      //Define as larguras.
       width: {
+        '564': '4.60rem',
         '1040': '65rem'
       },
+      //Define as larguras maximas.
       maxWidth: {
         'max-w-xxs': '19rem',
         'max-w-8xl': '90rem'
       },
+      //Define os espaçamentos
       spacing:{
         '25px': '1.563',
         '13px': '0.813'
